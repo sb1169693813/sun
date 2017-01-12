cd2c75f0b10f81dc48772680539a42c1b14d946c
sb1169693813 <446352377@qq.com>
2017-01-12 05:16:27 +0000

    Finish layout and routes 2017/1/12 13:16

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,8 @@
 */
 
 Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+//在Laravel中，我们可以通过在路由后面链式调用name方法来为路由指定名称：
+Route::get('/about', 'StaticPagesController@about')->name('about');
+//用户注册
+Route::get('/signup','UserController@create')->name('signup');
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,17 @@
 <head>
 	<meta charset="UTF-8">
 	<title>@yield('title','Sun App')suntest</title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-	@yield('content')
+<!-- header, nav 是 HTML5 提供的一种语义化标签，其实际作用与 div 一致，
+语义化的标签能帮助机器更方便理解代码，使代码更简洁，有助于网站的 SEO 优化。
+我们在上面代码使用到一些如 navbar, container 等类名在 Bootstrap 中都拥有特殊含义。 -->
+	@include('layouts._header')
+	<div class="container">
+      @yield('content')
+      @include('layouts._footer')
+      <!-- @  include 是 Blade 提供的视图引用方法，可通过传送一个具体的文件路径名称来引用视图。 -->
+    </div>
 </body>
 </html>
